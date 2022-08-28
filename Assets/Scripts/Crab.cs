5a48783cde4312a71ce352e5bc96e24ab6d94000
Karlsson0214 <karlsson0214@gmail.com>
2022-08-28 15:31:25 +0200

    on collision randomly turn left och right

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CapsuleCollider2D))]
@@ -23,6 +24,33 @@ public class Crab : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // reset speed
+        float angleRadians = rb.rotation * Mathf.PI / 180f;
+        float xSpeed = Mathf.Cos(angleRadians) * speed;
+        float ySpeed = Mathf.Sin(angleRadians) * speed;
+        rb.velocity = new Vector2(xSpeed, ySpeed);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // back up - to prevent colliders from overlapping
+        transform.position -= transform.right.normalized * speed * Time.deltaTime * 2;
+
+        // random left och right
+        if (Random.Range(0, 2) == 0)
+        {
+            // turn left
+            rb.rotation += Random.Range(40, 90);
+        }
+        else
+        {
+            // turn right
+            rb.rotation -= Random.Range(40, 90);
+        }
         
+        // reset speed
+        float angleRadians = rb.rotation * Mathf.PI / 180f;
+        float xSpeed = Mathf.Cos(angleRadians) * speed;
+        float ySpeed = Mathf.Sin(angleRadians) * speed;
+        rb.velocity = new Vector2(xSpeed, ySpeed);
     }
 }
