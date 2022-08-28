ca1b3f894f483af1797bf332cd6f9740f69f84c7
Karlsson0214 <karlsson0214@gmail.com>
2022-08-28 14:53:03 +0200

    Crab added to world

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 
 public class Crab : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float speed = 4;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        // Physics enginge will not rotate Crab
+        rb.freezeRotation = true;
+        // remove gravity
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(speed, 0);
     }
 
     // Update is called once per frame
