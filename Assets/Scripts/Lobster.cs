29d9c1bf0898d6e81ac70926c32b840d9ce123be
Karlsson0214 <karlsson0214@gmail.com>
2022-08-28 16:05:18 +0200

    Move forward. Turn on collision.

--- Assets/Scripts/Lobster.cs
+++ Assets/Scripts/Lobster.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class Lobster : MonoBehaviour
 {
+    private float speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        // not rotated by the Physics engine
+        rb.freezeRotation = true;
+        // not effected by gravity
+        rb.gravityScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // move forward
+        transform.position += transform.right.normalized * speed * Time.deltaTime;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // rotate 30 degrees anticlockwise
+        transform.rotation *= Quaternion.AngleAxis(30, Vector3.forward);
     }
 }
