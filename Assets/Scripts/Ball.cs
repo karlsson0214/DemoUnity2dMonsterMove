49b787f4cc343f04abedaf9058a588cbbb78b808
Karlsson0214 <karlsson0214@gmail.com>
2022-08-31 13:04:59 +0200

    Use Rigidbody2D to detect collision. Then do not use transform.Translate(...) to move.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    private Vector3 speed = new Vector3(5, 0, 0);
+    private float speed = 5;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(speed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime);
+        // reset speed
+        rb.velocity = new Vector2(speed, 0);
     }
     private void OnCollisionEnter2D()
     {
         speed *= -1;
-        //speed = new Vector3(-5, 0, 0);
-        Debug.Log("collision");
 
     }
 }
